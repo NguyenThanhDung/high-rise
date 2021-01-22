08b4c70a445023b7d57d7824e35e1eb56d7d0441
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2021-01-22 23:05:10 +0700

    Instantiate some other cubes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private MeshRenderer cubePrefab;
-    [SerializeField] private Material material;
+    [SerializeField] private Material[] materials;
 
     void Start()
     {
-        var cube = Instantiate(this.cubePrefab);
-        var meshRenderer = cube.GetComponent<MeshRenderer>();
-        meshRenderer.material = this.material;
+        for(int i = 0; i < materials.Length; i++)
+        {
+            var cube = Instantiate(this.cubePrefab, Vector3.right * i, Quaternion.identity);
+            var meshRenderer = cube.GetComponent<MeshRenderer>();
+            meshRenderer.material = this.materials[i];
+        }
     }
 }
