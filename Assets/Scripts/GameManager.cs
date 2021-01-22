bc1041ba09d58ee74204d0f9d7ae5b5291917169
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2021-01-22 23:00:23 +0700

    Set material for the first cube

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private MeshRenderer cubePrefab;
+    [SerializeField] private Material material;
 
     void Start()
     {
-        Instantiate(this.cubePrefab);
+        var cube = Instantiate(this.cubePrefab);
+        var meshRenderer = cube.GetComponent<MeshRenderer>();
+        meshRenderer.material = this.material;
     }
 }
