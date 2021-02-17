1b01bc1745f91d40664457556f5f29063b68df20
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2021-02-17 17:34:29 +0700

    Rotate the camera by the mouse

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 100f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float verticalRotation = (Input.GetAxis("Mouse Y") * (-this.rotateSpeed) * Time.deltaTime);
+            this.transform.Rotate(this.transform.right, verticalRotation, Space.World);
+            float horizontalRotation = Input.GetAxis("Mouse X") * this.rotateSpeed * Time.deltaTime;
+            this.transform.Rotate(Vector3.up, horizontalRotation, Space.World);
+        }
+    }
+}
