8567b72e5d407f9099461b8b94fbe7e919c69162
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2021-03-18 21:29:28 +0700

    Add CubeStackManager to manage CubeStacks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,12 @@ public class GameManager : MonoBehaviour
 {
     public const int MAX_CUBES_QUEUE_SIZE = 3;
 
+    public int boardSize;
+    public Color[] colors;
+
     [SerializeField] private GameObject placeholderPrefab;
     [SerializeField] private MeshRenderer cubePrefab;
-    [SerializeField] private Color[] colors;
-    [SerializeField] private int boardSize;
 
-    private List<Vector2> emptyLocations;
     private Queue<Color> cubesQueue;
 
     public Action<Queue<Color>> OnUpdateCubesQueue;
@@ -20,14 +20,6 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         SpawnPlaceholders();
-        InitializeEmptyLocation();
-        for(int i = 0; i < this.colors.Length; i++)
-        {
-            var location = PopRandomLocation();
-            var cube = Instantiate(this.cubePrefab, new Vector3(location.x, 0, location.y), Quaternion.identity);
-            var meshRenderer = cube.GetComponent<MeshRenderer>();
-            meshRenderer.material.color = this.colors[i];
-        }
         InitializeCubesQueue();
         OnUpdateCubesQueue(this.cubesQueue);
     }
@@ -57,27 +49,6 @@ public class GameManager : MonoBehaviour
                 Instantiate(this.placeholderPrefab, new Vector3(i, -0.5f, j), Quaternion.Euler(90, 0, 0));
             }
         }
-    }
-
-    private void InitializeEmptyLocation()
-    {
-        this.emptyLocations = new List<Vector2>();
-        for(int i = 0; i < this.boardSize; i++)
-        {
-            for(int j = 0; j < this.boardSize; j++)
-            {
-                Vector2 newLocation = new Vector2(i, j);
-                this.emptyLocations.Add(newLocation);
-            }
-        }
-    }
-
-    private Vector2 PopRandomLocation()
-    {
-        int index = UnityEngine.Random.Range(0, this.emptyLocations.Count);
-        var location = this.emptyLocations[index];
-        this.emptyLocations.RemoveAt(index);
-        return location;
     }
 
     private void InitializeCubesQueue()
