534bccc41b6ef2c97c882494eefe9267c6c30670
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-09-19 22:41:21 -0500

    Instantiate cube when pillar is instantiated

--- Assets/Scripts/Models/Pillar.cs
+++ Assets/Scripts/Models/Pillar.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Pillar : MonoBehaviour
 {
+    [SerializeField]
+    private Cube cubePrefab;
+
     private List<Cube> cubes;
 
     void Awake()
     {
         this.cubes = new List<Cube>();
+    }
+
+    void Start()
+    {
+        var cube = Instantiate(this.cubePrefab, this.transform.position + Vector3.up * 0.5f, Quaternion.identity, this.transform);
     }
 }
