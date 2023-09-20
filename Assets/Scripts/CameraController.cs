de44f155ff972821b85f0d352538b5848c447851
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-09-19 22:13:52 -0500

    Refactor CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,10 +10,17 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            float verticalRotation = (Input.GetAxis("Mouse Y") * (-this.rotateSpeed) * Time.deltaTime);
-            this.transform.Rotate(this.transform.right, verticalRotation, Space.World);
-            float horizontalRotation = Input.GetAxis("Mouse X") * this.rotateSpeed * Time.deltaTime;
-            this.transform.Rotate(Vector3.up, horizontalRotation, Space.World);
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            Rotate(mouseX, mouseY);
         }
+    }
+
+    private void Rotate(float mouseX, float mouseY)
+    {
+        float horizontalRotation = mouseX * this.rotateSpeed * Time.deltaTime;
+        this.transform.Rotate(Vector3.up, horizontalRotation, Space.World);
+        float verticalRotation = mouseY * (-this.rotateSpeed) * Time.deltaTime;
+        this.transform.Rotate(this.transform.right, verticalRotation, Space.World);
     }
 }
