8d5cfee2a9739d8e6fc497653bfbd74e3dca7a83
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-04 20:52:24 -0500

    Send the location of the source board slot

--- Assets/Scripts/Models/Board.cs
+++ Assets/Scripts/Models/Board.cs
@@ -4,6 +4,9 @@ public class Board : MonoBehaviour
 {
     public static Board Instance { get; private set; }
 
+    [SerializeField]
+    private GameObject boardSlotPrefab;
+
     private BoardSlot[,] boardSlots;
     private ScoreManager scoreManager;
 
@@ -18,10 +21,20 @@ public class Board : MonoBehaviour
     void Start()
     {
         this.boardSlots = new BoardSlot[GameManager.Instance.boardSize, GameManager.Instance.boardSize];
+
+        for (int i = 0; i < GameManager.Instance.boardSize; i++)
+        {
+            for (int j = 0; j < GameManager.Instance.boardSize; j++)
+            {
+                var boardSlotObj = Instantiate(this.boardSlotPrefab, new Vector3(i, -0.5f, j), Quaternion.Euler(90, 0, 0));
+                var boardSlot = boardSlotObj.GetComponent<BoardSlot>();
+                boardSlot.SetLocation(i, j);
+            }
+        }
     }
 
-    public void OnPuttingPillar()
+    public void OnPuttingPillar(BoardSlot boardSlot)
     {
-        Debug.Log("Board.OnPuttingPillar()");
+        Debug.Log($"Board.OnPuttingPillar() location:({boardSlot.Row}, {boardSlot.Column})");
     }
 }
--- Assets/Scripts/Models/BoardSlot.cs
+++ Assets/Scripts/Models/BoardSlot.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class BoardSlot : MonoBehaviour
 {
     [SerializeField]
-    private Pillar pillarPrefab;
+    private Pillar _pillarPrefab;
+
+    private int[] _localtion;
+
+    public int Row { get { return _localtion[0]; } }
+    public int Column { get { return _localtion[1]; } }
+
+    void Awake()
+    {
+        _localtion = new int[2];
+    }
+
+    public void SetLocation(int row, int col)
+    {
+        _localtion[0] = row;
+        _localtion[1] = col;
+    }
 
     public void OnTap()
     {
         Debug.Log("Tapping slot");
-        Instantiate(pillarPrefab, this.transform);
-        Board.Instance.OnPuttingPillar();
+        Instantiate(_pillarPrefab, this.transform);
+        Board.Instance.OnPuttingPillar(this);
     }
 
     public void Combine(BoardSlot other)
